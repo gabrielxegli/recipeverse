69c76182e5805bf46c015e7ade23b4b5f904acca
Gabriel Egli <mail@gabriel-egli.ch>
2023-07-06 16:44:47 +0200

    fetched images with title from api

--- app/src/main/java/ch/zkb/recipeverse/control/Fetch.java
+++ app/src/main/java/ch/zkb/recipeverse/control/Fetch.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -60,4 +61,5 @@ public class Fetch extends Service {
 
         queue.add(jsonObjectRequest);
     }
+
 }
--- app/src/main/java/ch/zkb/recipeverse/view/MainActivity.java
+++ app/src/main/java/ch/zkb/recipeverse/view/MainActivity.java
@@ -6,8 +6,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,12 +20,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
 import ch.zkb.recipeverse.R;
 import ch.zkb.recipeverse.control.Fetch;
+
+import com.bumptech.glide.Glide;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,15 +38,17 @@ public class MainActivity extends AppCompatActivity {
     boolean succesfull = false;
 
     TextView text;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = findViewById(R.id.main_text);
+        ctx = this;
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        layout = findViewById(R.id.saved_layout);
+
 
 
 
@@ -64,16 +74,54 @@ public class MainActivity extends AppCompatActivity {
 
             String[][] queries = {
                     {
-                            "apiKey", "0b3d242f0f2c41e69d12fbfb2b866864"
+                        "apiKey", "0b3d242f0f2c41e69d12fbfb2b866864"
                     },
                     {
-                            "query", "pasta"
+                        "query", "noodles"
+                    },
+                    {
+                        "number", "25"
                     }
             };
 
             try {
                 fetch.get("https://api.spoonacular.com/recipes/complexSearch", queries, (JSONObject json)->{
-                    text.setText(json.toString());
+                    try {
+                        JSONArray jsonArray = json.getJSONArray("results");
+
+                        int len = jsonArray.length();
+
+                        for (int i = 0; i < len; i++) {
+                            JSONObject entry = jsonArray.getJSONObject(i);
+
+                            String title = entry.getString("title");
+                            String image = entry.getString("image");
+
+                            ImageView imageView = new ImageView(ctx);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(500, 500);
+
+                            params.setMargins(5,5,5,5);
+
+                            imageView.setLayoutParams(params);
+
+                            TextView text = new TextView(ctx);
+
+                            text.setText(title);
+
+                            LinearLayout container = new LinearLayout(ctx);
+
+                            container.setOrientation(LinearLayout.VERTICAL);
+
+                            container.addView(imageView);
+                            container.addView(text);
+
+                            layout.addView(container);
+
+                            Glide.with(ctx).load(image).centerCrop().into(imageView);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
             } catch (Exception e) {
                 throw new RuntimeException(e);
